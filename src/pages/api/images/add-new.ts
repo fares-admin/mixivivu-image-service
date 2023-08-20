517e9f3998ca5270c23d5002544398c7cd8b5ee6
fares-admin <fares.sys.vn@gmail.com>
2023-08-20 23:54:45 +0700

    add cors

--- src/pages/api/images/add-new.ts
+++ src/pages/api/images/add-new.ts
@@ -1,8 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-import { ImageService } from '@/src/service/image-service/image-service'
 import { InternalAuthService } from '@/src/service/internal-auth-service/internal-auth-service'
-import { wrapperEndpoint } from 'common-abstract-fares-system'
 
 /*
     @ericchen:
@@ -20,7 +18,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const internalService = new InternalAuthService()
   const authResult = await internalService.authUserToken(req.headers.authorization || '')
   res.setHeader('Access-Control-Allow-Credentials', 'true')
-  res.setHeader('Access-Control-Allow-Origin', '*') // replace this your actual origin
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    req.headers.host?.includes('localhost')
+      ? `http://${req.headers.host}`
+      : `https://${req.headers.host}`
+  ) // replace this your actual origin
   res.setHeader('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
   res.setHeader(
     'Access-Control-Allow-Headers',
@@ -29,8 +32,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (!authResult.success) {
     res.status(200).json(authResult)
   } else {
-    const service = new ImageService()
-    const result = await wrapperEndpoint(req, 'POST', service.addNewImage(req))
-    res.status(200).json(result)
+    // const service = new ImageService()
+    // const result = await wrapperEndpoint(req, 'POST', service.addNewImage(req))
+    // res.status(200).json(result)
   }
 }
