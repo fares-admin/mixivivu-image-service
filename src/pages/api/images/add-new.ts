184026f90fad68dc58544b1ccd75e548adb16e54
fares-admin <fares.sys.vn@gmail.com>
2023-08-20 19:34:20 +0700

    add cors

--- src/pages/api/images/add-new.ts
+++ src/pages/api/images/add-new.ts
@@ -1,7 +1,8 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
 import { ImageService } from '@/src/service/image-service/image-service'
 import { InternalAuthService } from '@/src/service/internal-auth-service/internal-auth-service'
 import { wrapperEndpoint } from 'common-abstract-fares-system'
-import { NextApiRequest, NextApiResponse } from 'next'
 
 /*
     @ericchen:
@@ -18,10 +19,18 @@ export const config = {
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const internalService = new InternalAuthService()
   const authResult = await internalService.authUserToken(req.headers.authorization || '')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Allow-Origin', '*') // replace this your actual origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   if (!authResult.success) {
     res.status(200).json(authResult)
+  } else {
+    const service = new ImageService()
+    const result = await wrapperEndpoint(req, 'POST', service.addNewImage(req))
+    res.status(200).json(result)
   }
-  const service = new ImageService()
-  const result = await wrapperEndpoint(req, 'POST', service.addNewImage(req))
-  res.status(200).json(result)
 }
--- src/pages/api/images/get-list.ts
+++ src/pages/api/images/get-list.ts
@@ -1,6 +1,7 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
 import { ImageService } from '@/src/service/image-service/image-service'
 import { wrapperEndpoint } from 'common-abstract-fares-system'
-import { NextApiRequest, NextApiResponse } from 'next'
 
 /*
     @ericchen:
@@ -11,5 +12,12 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const service = new ImageService()
   const result = await wrapperEndpoint(req, 'GET', service.getListImage(req))
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Allow-Origin', '*') // replace this your actual origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   res.status(200).json(result)
 }
